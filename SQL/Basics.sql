0d4c86860a7b29949aad64496114aa250d5ec6fe
YenIndra <100924093+YenIndra@users.noreply.github.com>
2022-09-11 11:45:20 +0530

    Update Basics.sql

--- SQL/Basics.sql
+++ SQL/Basics.sql
@@ -1,1 +1,11 @@
+SQL : Structured Query Language.
+---> Types OF SQL Commands :
+1.DDL : Data Definition Language. (CREATE, DROP, ALTER, TRUNCATE) -> Defines Structure of Database.
+2.DML : Data Manipulation Language. (INSERT, UPDATE, DELETE, MERGE) -> 
+3.DCL : Data Control language. (GRANT, REVOKE)
+4.TCL : Transaction Control Language. (COMMIT, ROLLBACK, SAVEPOINT)
+5.DQL : Data Query language. (SELECT)
 
+
+Data Types :Varchar, Int, Date, Float, Boolean.
+Constraints : Check, Not Null, Unique, Primary Key, Foreign Key,
